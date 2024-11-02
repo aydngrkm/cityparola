567e88765129fd33cbea0f72c95040ddfd6b24e9
aydngrkm <gorkemaydinn@gmail.com>
2024-11-02 14:06:31 +0300

    Implement JWT and corsheaders
    - Implement JWT tokens
    - Set endpoints to get tokens
    - Set access token lifetime to 5 minutes and refresh token lifetime to 90 days
    - Allow all origins to make requests for now
    - Update requirements.txt

--- back-end/cityparola/api/urls.py
+++ back-end/cityparola/api/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('countries/', views.CountryList.as_view()),
     path('countries/<int:pk>/', views.CountryDetail.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
